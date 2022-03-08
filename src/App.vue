9fed13476b35b79409de6e1c3dfcf2d731134792
Afzalbek <uzafo.dev@gmail.com>
2022-03-08 09:52:48 +0500

    VueJs site

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Navbar from './components/navbar/Navbar.vue'
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<template>
+<Navbar />
+</template>
+
+<style src="./assets/main.css"></style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <h6 class="text-white bgg text-center p1 pf w-100 fs13">Vue 3 is now the new default version! Learn more</h6>
+    <div class="main-nav">
+        <div class="max-big-w">
+            <div class="first-nav">
+                <svg class="logo" viewBox="0 0 128 128" width="24" height="24" data-v-5f26462c=""><path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z" data-v-5f26462c=""></path><path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z" data-v-5f26462c=""></path></svg>
+                Vue.js
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.main-nav {
+    position: fixed;
+    top: 21.7px;
+    width: 100%;
+    border-bottom: 1px solid #dbdbdb;
+    margin: 0;
+    padding: 0;
+}
+.first-nav {
+    display: flex;
+    width: 45%;
+    justify-content: flex-start;
+    align-items: center;
+    font-weight: 550;
+    font-size: 18px;
+}
+.logo {
+    margin-right: 10px;
+}
+</style>
